a2c91999bc2171d616a21892e802a9ecf16cde8b
sergeybaldin98 <57862342+sergeybaldin98@users.noreply.github.com>
2022-01-19 22:37:18 +0300

    docs: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  var a=Math.abs(n);
+    num = String (a);
+    var arr = [];
+    for (var i = num.length; i>=0; i--){
+        arr.push(num[i]);
+    }
+    str = arr.join('');
+    let m = Number (str);
+    return m;
 }
